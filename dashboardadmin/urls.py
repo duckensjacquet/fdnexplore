fc0fd6f4fd9435c2e2918c0b081788b38d2ce566
Duckenson jacquet <duckens244@hotmail.com>
2022-11-30 12:45:02 -0500

    Gestion user

--- dashboardadmin/urls.py
+++ dashboardadmin/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('',views.Dashboard),
      path('newarticle',views.Newarticle,name='newarticle'),
      path('article',views.ListArticle,name='article'),
+     path('adduser',views.AddUser,name='adduser'),
+     path('users',views.ListUsers,name='users')
     
 ]
--- dashboardadmin/views.py
+++ dashboardadmin/views.py
@@ -15,6 +15,10 @@ def ListArticle(request):
     article=Article.objects.all().order_by("-id")
     return render(request, 'dashboardadmin/articles.html', {'articles': article})
 
+def ListUsers(request):
+    users=User.objects.all()
+    return render(request, 'dashboardadmin/listuser.html', {'users': users})
+
 def Newarticle(request):
     category=Category.objects.all()
     article=Article()
@@ -24,7 +28,32 @@ def Newarticle(request):
         article.imagethumbnail=request.FILES['imagethumbnail']
         article.publishedby=User.objects.get(pk=request.user.id)
         article.category=Category.objects.get(pk=int(request.POST['category']))
+        article.imagecaptions=request.POST['caption']
         article.save()
         article=Article.objects.all().order_by("-id")
         return HttpResponseRedirect(reverse('article'))
     return render(request, 'dashboardadmin/newarticle.html',{'categories': category})
+
+
+def AddUser(request):
+    if request.method == 'POST':
+        if len(request.POST['username']) < 0 or len(request.POST['password'])<6:
+            print('sorry')
+        else:
+            user=User()
+            user.first_name=request.POST['firstname']
+            user.last_name=request.POST['lastname']
+            user.username=request.POST['username']
+            user.email=request.POST['email']
+            user.password=request.POST['password']
+            if request.POST.get('staff','')=='on':
+                user.is_staff=True
+            else:
+                user.is_staff=False
+            if request.POST.get('superuser','')=='on':
+                user.is_superuser=True
+                user.is_staff=True
+            else:
+                user.is_superuser=False
+            user.save()
+    return render(request, 'dashboardadmin/adduser.html')
